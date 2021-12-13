9673ddf2b4346e85681f83d2ba6ebe787f01266e
Rakesh Chaudhary <arcsle09plus@gmail.com>
2021-12-13 11:21:09 +0000

    added enail notifier

--- scraper.py
+++ scraper.py
@@ -2,6 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.text import MIMEText
+import os
+import json
+
 
 youtube_video_url = "https://www.youtube.com/feed/trending"
 
@@ -34,6 +42,32 @@ def parse_video_information(video):
    'Description':description
   }
 
+def send_email(Filename,Body):
+  SENDER_EMAIL = 'arcsle09webscrape@gmail.com'
+  RECEIVER_EMAIL = 'arcsle09webscrape@gmail.com'
+  SENDER_PASSWORD = os.environ['SENDER_PASSWORD']
+  print(SENDER_PASSWORD)
+  msg = MIMEMultipart('mixed')
+  msg['Subject'] = 'Youtube Trending Videos'
+  msg['From'] = SENDER_EMAIL
+  msg['To'] = RECEIVER_EMAIL
+  text1 = MIMEText(Body,'plain')
+  msg.attach(text1)
+  attachment = MIMEBase('application',"octet-stream")
+  attachment.set_payload(open(Filename,"rb").read())
+  encoders.encode_base64(attachment)
+  attachment.add_header('content-disposition',"attachment", filename= Filename)
+  msg.attach(attachment)
+  try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(SENDER_EMAIL,SENDER_PASSWORD)
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.close()
+    print("Email Sent !!")
+  except:
+    print('Something went wrong...')
+
 if __name__== "__main__":
 
   #initiate the driver element
@@ -48,13 +82,17 @@ if __name__== "__main__":
 
   video_data = [parse_video_information(video) for video in videos[0:10]]
 
-  print("Saving the data to CSV")
+  print("Saving the data to excel")
 
   videos_df = pd.DataFrame(video_data)
 
   videos_df.to_csv('trending.csv',index=False)
 
-  #print(videos_df)
+  print("Sending an email with results")
+
+  Body = json.dumps(video_data,indent = 2)
+  send_email('trending.csv',Body)
+
 
 
 
